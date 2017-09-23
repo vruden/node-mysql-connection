b7412f807dbbdc4aae91ce33467193ea2b1b3fdd
Vladimir Rudenko <ruden.v.a@gmail.com>
2017-09-23 17:47:05 +0300

    TypeScript integration

--- test/index.js
+++ test/index.js
@@ -1,22 +1,59 @@
-var q = require('../index');
+let MysqlConnection = require('../lib/index');
+let async = require('async');
+let assert = require('assert');
 
-var main_db = q.createConnection({
+let mainDB = MysqlConnection.createConnection({
     name: 'main_db',
     connection: {
         host: 'localhost',
         user: 'root',
         password: '123456',
-        database: 'node-migrate'
+        database: 'node-test',
+        debug: false
     },
     customFormat: true,
-    TTL: 0, // Time To Live for a cache key in seconds (0 = infinite)
-    verbose: true, // Do you want console.log's about what the program is doing?
-    caching: true // Do you want to use SELECT SQL caching?
+    verbose: true
+});
+
+let q = mainDB.query('DROP TABLE connection');
+q.on('end', function () {
+    console.log('drop')
+
+    mainDB.query('CREATE TABLE connection (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT, note VARCHAR(255))', [], function (err, results, fields, sql) {
+        console.log(arguments)
+
+        let user = {name: 'Ivan', age: 12};
+
+        mainDB.query('INSERT INTO connection SET :user', {user: user}, function (err, results, fields, sql) {
+            console.log(arguments)
+        });
+
+        let users = [
+            ['Alex', 34],
+            ['Tom', 56],
+        ];
+
+        mainDB.query('INSERT INTO connection (name, age) VALUES :users', {users: users}, function (err, results, fields, sql) {
+            console.log(arguments)
+        });
+
+        mainDB.query('INSERT INTO connection (name, age) VALUES (:name, :age)', {name: 'Mary', age: 21}, function (err, results, fields, sql) {
+            console.log(arguments)
+        });
+
+        //  {ttl: 60},
+        mainDB.query('SELECT * FROM ::name WHERE name = :tt AND age > :age', {name: 'user', age: 18, note: 'first user'}, function (err, results, fields, queryString) {
+            console.log(results, queryString)
+
+            // process.exit(0)
+        });
+
+    });
+
 });
 
 
-main_db.query('select * from ::name where name = :tt ', {name: 'user', tt: 'Ivan'}, {ttl: 60}, function (err, result, fields, queryString) {
-    console.log(result, queryString)
 
-    process.exit(0)
-})
+
+
+
